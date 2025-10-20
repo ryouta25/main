5feaa8c39c9fdac196e04878f6da533a9e75e3a3
ryouta25 <avp22.07@mail.ru>
2025-10-21 00:22:37 +0300

    functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+int** create(size_t rows, size_t cols);
+void remove(int** m, size_t rows, size_t cols);
+void input(int** m, size_t rows, size_t cols);
+void output(const int* const* m, size_t rows, size_t cols);
+int** convert(const int* t, size_t n, const size_t* lns, size_t rows);
 
 int main(){
 	size_t rows = 0, cols = 0;
