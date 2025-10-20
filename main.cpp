5cbea2031cb7eb5925680916f1d139bd97df5b50
ryouta25 <avp22.07@mail.ru>
2025-10-21 00:30:48 +0300

    functions create and remove

--- main.cpp
+++ main.cpp
@@ -26,3 +26,29 @@ void output(const int* const* m, size_t rows, size_t cols) {
         std::cout << "\n";
     }
 }
+
+int** create(size_t rows, size_t cols) {
+    int** result = new int*[rows];
+    size_t i = 0;
+    try {
+        for (; i < rows; ++i) {
+            result[i] = new int[cols];
+        }
+    }
+    catch (...) {
+        for (size_t j = 0; j < i; ++j) {
+            delete[] result[j];
+        }
+        delete[] result;
+        throw;
+    }
+    return result;
+}
+
+void remove(int** m, size_t rows, size_t /*cols*/) {
+    if (m == nullptr) return;
+    for (size_t i = 0; i < rows; ++i) {
+        delete[] m[i];
+    }
+    delete[] m;
+}
