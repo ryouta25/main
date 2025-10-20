48048a16591bc4b6253d17c0f1ea09aa2707d86d
ryouta25 <avp22.07@mail.ru>
2025-10-21 00:27:30 +0300

    functions input and output

--- main.cpp
+++ main.cpp
@@ -14,3 +14,15 @@ int main(){
 	std::cout << rows << " "<< cols;
 	std::cout << "\n";
 }
+
+void input(int** m, size_t rows, size_t cols) {}
+
+void output(const int* const* m, size_t rows, size_t cols) {
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            std::cout << m[i][j];
+            if (j + 1 < cols) std::cout << " ";
+        }
+        std::cout << "\n";
+    }
+}
